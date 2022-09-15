506b019afbd5b22735070290601956d4212bc640
James <39100243+James5618@users.noreply.github.com>
2022-09-15 02:36:07 +0000

    Add files via upload

---
+++ engine/src/core/logger.c
@@ -0,0 +1,13 @@
+// Program Name: logger
+#include "logger.h"
+
+b8 initialize_logging() 
+{}
+
+void shutdown_logging() {
+
+}
+
+EAPI void log_output(log_output level, const char* message, ...) {
+
+}
